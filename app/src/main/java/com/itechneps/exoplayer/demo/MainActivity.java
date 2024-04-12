f137d8f1c65a503a36165d941f5350fee3c60957
Lila Ballabh Bhurtel <46861971+nepsalone@users.noreply.github.com>
2024-04-12 11:29:23 +0545

    forth commit

--- app/src/main/java/com/itechneps/exoplayer/demo/MainActivity.java
+++ app/src/main/java/com/itechneps/exoplayer/demo/MainActivity.java
@@ -1,8 +1,10 @@
-package com.itechneps.exoplayer;
+package com.itechneps.exoplayer.demo;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.itechneps.exoplayer.demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
